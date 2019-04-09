f5e945958098ddc71ffaadb75523eee7d4b8c5c8
bachlqth1807027 <bachlqth1807027@fpt.edu.vn>
2019-04-09 22:54:03 +0700

    nop bai assigment

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import utility.EmployeeMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeMenu menu = new EmployeeMenu();
+        menu.createMenu();
+    }
+}
